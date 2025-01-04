ccc283da3f489f2c232e6dec0678b397087ef901
JPDas <jprakash.das@gmail.com>
2025-01-04 11:24:23 +0530

    Added pytest calling fastapi home

--- test/test_operations.py
+++ test/test_operations.py
@@ -8,4 +8,4 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code==200
-    assert response.data== b"Hello World!"
+    assert response.json()== {"msg": "Hello World!"}
