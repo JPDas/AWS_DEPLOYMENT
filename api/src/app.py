bd649e4a20c6bb14d3a15153c1d51fd45eed724c
JPDas <jprakash.das@gmail.com>
2025-01-05 15:08:23 +0530

    Restructuring the code

---
+++ api/src/app.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI(title="AWS + FastAPI",
+    description="AWS API Gateway, Lambdas and FastAPI (oh my)")
+
+# Mangum allows us to use Lambdas to process requests
+handler = Mangum(app=app)
+
+@app.get("/")
+async def root():
+   return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+   uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
---
+++ api/test/test_operations.py
@@ -0,0 +1,11 @@
+import pytest
+from src.app import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+
+    assert response.status_code==200
+    assert response.json()== {"message": "Hello World!"}
