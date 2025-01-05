91018e2f0d44d9b4df1a0bdd1e4e8834fb25cb40
JPDas <jprakash.das@gmail.com>
2025-01-05 22:29:37 +0530

    Addedd proper app in uvicorn

--- app/app.py
+++ app/app.py
@@ -13,4 +13,4 @@ async def root():
    return {"message": "Hello World!"}
 
 if __name__ == "__main__":
-   uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+   uvicorn.run(app, host="0.0.0.0", port=8000)
