1628a5cbf701e9a126d8be13b596cbdab1484499
JPDas <jprakash.das@gmail.com>
2025-01-05 15:34:41 +0530

    Restructuring the code

--- api/src/app.py
+++ api/src/app.py
@@ -12,5 +12,6 @@ handler = Mangum(app=app)
 async def root():
    return {"message": "Hello World!"}
 
+
 if __name__ == "__main__":
    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
